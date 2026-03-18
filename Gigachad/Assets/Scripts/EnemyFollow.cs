06e40077cb431b4fc2976c14e792de6abce65996
Louiesdad42 <michael.chiodo@digipen.edu>
2026-03-18 13:30:14 -0700

    Added a public GameObject to the enemy follow

--- Gigachad/Assets/Scripts/EnemyFollow.cs
+++ Gigachad/Assets/Scripts/EnemyFollow.cs
@@ -2,6 +2,8 @@ using UnityEngine;
 
 public class EnemyFollow : MonoBehaviour
 {
+    public GameObject player;
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
