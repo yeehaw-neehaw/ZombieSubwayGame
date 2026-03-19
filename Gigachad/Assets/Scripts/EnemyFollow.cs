d6579e90ef0d064d41ca5e7fe462ecd2048b08cb
Louiesdad42 <michael.chiodo@digipen.edu>
2026-03-19 14:04:46 -0700

    Comments, Enemy Following, and Enemy Spawning
    Commented on Player controlling scripts and created scripts for enemy following and enemy spawning, as well as having a manipulatable level timer and spawn timer

--- Gigachad/Assets/Scripts/EnemyFollow.cs
+++ Gigachad/Assets/Scripts/EnemyFollow.cs
@@ -1,18 +1,29 @@
+/****************************************************************************
+* File Name: EnemyFollow.c
+* Author: Michael Chiodo
+* DigiPen Email: michael.chiodo@digipen.edu
+* Course: Game Projects
+*
+* Description: Instructs anything this script is attached to to follow after
+* another transform.
+*
+****************************************************************************/
+
 using UnityEngine;
 
 public class EnemyFollow : MonoBehaviour
 {
-    public GameObject player;
-
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-        
-    }
+    public Transform playerPos;
+    public Transform selfPos;
+    private Vector3 direction;
+    private Rigidbody2D myRb;
+    public float followSpeed = 0.5f;
 
     // Update is called once per frame
     void Update()
     {
-        
+        myRb = gameObject.GetComponent<Rigidbody2D>();
+        direction = playerPos.position - selfPos.position;
+        myRb.linearVelocity = direction * followSpeed;
     }
 }
---
+++ Gigachad/Assets/Scripts/EnemySpawning.cs
@@ -0,0 +1,39 @@
+/****************************************************************************
+* File Name: EnemySpawning.c
+* Author: Michael Chiodo
+* DigiPen Email: michael.chiodo@digipen.edu
+* Course: Game Projects
+*
+* Description: Spawns enemies at a set y coordinate, with a random x coordinate.
+* Must be adjusted when we have the actual y coordinate for the barricades.
+*
+****************************************************************************/
+
+using UnityEngine;
+
+public class EnemySpawning : MonoBehaviour
+{
+    public float timer = 1f;
+    private float countdown = 1f;
+    private int randomInt;
+    public GameObject enemyPrefab;
+    public float levelTimer = 50f;
+    public float levelCountdown = 0f;
+
+    // Update is called once per frame
+    void Update()
+    {
+        levelCountdown += Time.deltaTime;
+        randomInt = Random.Range(-12, 12);
+        if (countdown >= timer && levelCountdown < levelTimer)
+        {
+            //Instantiates a new enemy at a set y coordinate
+            GameObject enemy = Instantiate(enemyPrefab, new Vector3(randomInt, -5, 0), Quaternion.Euler(0, 0, 0));
+            countdown = 0f;
+        }
+        else if (countdown < timer)
+        {
+            countdown += Time.deltaTime;
+        }
+    }
+}
