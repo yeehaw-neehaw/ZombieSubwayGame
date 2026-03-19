7f32d76e5d7eadf8345866427313168184120764
Louiesdad42 <michael.chiodo@digipen.edu>
2026-03-19 14:54:58 -0700

    Created a Script for Camera Following coding

---
+++ Gigachad/Assets/Scripts/CameraFollow.cs
@@ -0,0 +1,26 @@
+/****************************************************************************
+* File Name: CameraFollow.c
+* Author: Michael Chiodo
+* DigiPen Email: michael.chiodo@digipen.edu
+* Course: Game Projects
+*
+* Description: Instructs the camera to follow a game object around
+*
+****************************************************************************/
+
+using UnityEngine;
+
+public class CameraFollpw : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
