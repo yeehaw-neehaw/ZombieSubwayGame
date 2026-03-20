36df077339ec2379db07c5796603874179831f68
yeehaw-neehaw <neha.sankarkumar@digipen.edu>
2026-03-20 12:31:05 -0700

    comments + enemy disappear on fire

--- Gigachad/Assets/Scripts/TicketScript.cs
+++ Gigachad/Assets/Scripts/TicketScript.cs
@@ -1,14 +1,21 @@
+/****************************************************************************
+* File Name: TicketScript
+* Author: Neha Sankarkumar
+* DigiPen Email: neha.sankarkumar@digipen.edu
+* Course: Video Game Programming (Game Projects)
+*
+* Description: This code switches scenes upon a button being clicked. 
+****************************************************************************/
 using UnityEngine;
 
 public class TicketScript : MonoBehaviour
 {
     public GameObject Ticket;
-
-    void OnCollisionEnter2D(Collision2D collision)
+    void OnCollisionEnter2D(Collision2D collision) // when the ticket collides
     {
-        if (collision.gameObject.CompareTag("Player"))
+        if (collision.gameObject.CompareTag("Player")) // collides with player
         {
-            Destroy(Ticket);
+            Destroy(Ticket); // ticket disappears
         }
     }
 }
