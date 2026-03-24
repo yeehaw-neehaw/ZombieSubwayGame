d98d7ae71965c72ed5dd2f18eff69e59e42d3f22
yeehaw-neehaw <neha.sankarkumar@digipen.edu>
2026-03-24 14:03:31 -0700

    tickets spawn multiple times

--- Gigachad/Assets/Scripts/TicketSpawner.cs
+++ Gigachad/Assets/Scripts/TicketSpawner.cs
@@ -13,19 +13,23 @@ using UnityEngine;
 public class TicketSpawner : MonoBehaviour
 {
     public GameObject TicketPrefab;
-    bool TicketSpawned = false;
-    private float TimerMax = 5f; // TIMERMAX seconds elapse before a ticket will spawn
+    public static bool TicketSpawned = false;
+    private float TimerMax = 3f; // TIMERMAX seconds elapse before a ticket will spawn
     private float TimerCounter = 0f; // the actual timer
+    public static int TicketsCollected = 0;
+    public int TicketsNeeded = 5;
+    public int TicketsCreated = 0;
 
     // Update is called once per frame
     void Update()
     {
-        if (TimerCounter < TimerMax) // if the timer has not yet reached TIMERMAX seconds
+        if ((TimerCounter < TimerMax) && !TicketSpawned) // if the timer has not yet reached TIMERMAX seconds
         {
             TimerCounter += Time.deltaTime; // update timer
         }
-        else if (!TicketSpawned && (TimerCounter >= TimerMax)) // if the timer has reached TIMERMAX and a ticket does not already exist
+        else if (!TicketSpawned && (TimerCounter >= TimerMax) && TicketsCreated < TicketsNeeded) // if the timer has reached TIMERMAX and a ticket does not already exist
         {
+            TimerCounter = 0; // reset timer
             switch (UnityEngine.Random.Range(1, 9)) // randomly pick 1-8, instantiate a ticket in one of 8 spots
             {
                 case 1:
@@ -53,10 +57,42 @@ public class TicketSpawner : MonoBehaviour
                     Instantiate(TicketPrefab, new Vector3(4, -3, 0), Quaternion.identity);
                     break;
             }
+            TicketsCreated += 1;
 
-            TicketSpawned = true; // the ticket HAS spawned -- prevents infinite cloning
+            TicketSpawned = true; // the current ticket HAS spawned -- prevents infinite cloning
         }
         
         
+    }
+
+    void SpawnTicket()
+    {
+        switch (UnityEngine.Random.Range(1, 9)) // randomly pick 1-8, instantiate a ticket in one of 8 spots
+        {
+            case 1:
+                Instantiate(TicketPrefab, new Vector3(9, 3, 0), Quaternion.identity);
+                break;
+            case 2:
+                Instantiate(TicketPrefab, new Vector3(4, 3, 0), Quaternion.identity);
+                break;
+            case 3:
+                Instantiate(TicketPrefab, new Vector3(-4, 3, 0), Quaternion.identity);
+                break;
+            case 4:
+                Instantiate(TicketPrefab, new Vector3(-9, 3, 0), Quaternion.identity);
+                break;
+            case 5:
+                Instantiate(TicketPrefab, new Vector3(-9, -3, 0), Quaternion.identity);
+                break;
+            case 6:
+                Instantiate(TicketPrefab, new Vector3(-4, -3, 0), Quaternion.identity);
+                break;
+            case 7:
+                Instantiate(TicketPrefab, new Vector3(4, -3, 0), Quaternion.identity);
+                break;
+            case 8:
+                Instantiate(TicketPrefab, new Vector3(4, -3, 0), Quaternion.identity);
+                break;
+        }
     }
 }
