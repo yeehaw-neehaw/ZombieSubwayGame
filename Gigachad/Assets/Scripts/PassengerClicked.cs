8b91188c7fa1b8917bd62d382d2c67a28d629a61
Louiesdad42 <michael.chiodo@digipen.edu>
2026-03-26 13:57:40 -0700

    Made the shop work
    7 different passengers that can be clicked that each open different shops, can x out of each, cannot open 2 at a time

--- Gigachad/Assets/Scripts/PassengerClicked.cs
+++ Gigachad/Assets/Scripts/PassengerClicked.cs
@@ -1,31 +1,97 @@
 using UnityEngine;
+using UnityEngine.UI;
 
 public class PassengerClicked : MonoBehaviour
 {
-    public GameObject Upgrade1, Text1, Background1;
-    private bool clicked1 = false;
+    public GameObject Text1, Background1;
+    public Button Upgrader, xOut;
+    private static bool clicked = false;
     void Start()
     {
-        Upgrade1.gameObject.SetActive(false);
+        Upgrader.gameObject.SetActive(false);
         Text1.gameObject.SetActive(false);
         Background1.gameObject.SetActive(false);
+        xOut.gameObject.SetActive(false);
+        Upgrader.onClick.AddListener(OnButtonClick);
+        xOut.onClick.AddListener(OnXClick);
     }
     private void OnMouseDown()
     {
-        if (gameObject.name == "Passenger1" && !clicked1)
+        if (gameObject.name == "Passenger1" && !clicked)
         {
-            Upgrade1.gameObject.SetActive(true);
+            Upgrader.gameObject.SetActive(true);
             Text1.gameObject.SetActive(true);
             Background1.gameObject.SetActive(true);
-            clicked1 = true;
+            xOut.gameObject.SetActive(true);
+            clicked = true;
         }
-        else if (gameObject.name == "Passenger1" && clicked1)
+        if (gameObject.name == "Passenger2" && !clicked)
         {
-            Upgrade1.gameObject.SetActive(false);
-            Text1.gameObject.SetActive(false);
-            Background1.gameObject.SetActive(false);
-            clicked1 = false;
+            Upgrader.gameObject.SetActive(true);
+            Text1.gameObject.SetActive(true);
+            Background1.gameObject.SetActive(true);
+            xOut.gameObject.SetActive(true);
+            clicked = true;
         }
-        Debug.Log("You clicked on: " + gameObject.name);
+        if (gameObject.name == "Passenger3" && !clicked)
+        {
+            Upgrader.gameObject.SetActive(true);
+            Text1.gameObject.SetActive(true);
+            Background1.gameObject.SetActive(true);
+            xOut.gameObject.SetActive(true);
+            clicked = true;
+        }
+        if (gameObject.name == "Passenger4" && !clicked)
+        {
+            Upgrader.gameObject.SetActive(true);
+            Text1.gameObject.SetActive(true);
+            Background1.gameObject.SetActive(true);
+            xOut.gameObject.SetActive(true);
+            clicked = true;
+        }
+        if (gameObject.name == "Passenger5" && !clicked)
+        {
+            Upgrader.gameObject.SetActive(true);
+            Text1.gameObject.SetActive(true);
+            Background1.gameObject.SetActive(true);
+            xOut.gameObject.SetActive(true);
+            clicked = true;
+        }
+        if (gameObject.name == "Passenger6" && !clicked)
+        {
+            Upgrader.gameObject.SetActive(true);
+            Text1.gameObject.SetActive(true);
+            Background1.gameObject.SetActive(true);
+            xOut.gameObject.SetActive(true);
+            clicked = true;
+        }
+        if (gameObject.name == "Passenger7" && !clicked)
+        {
+            Upgrader.gameObject.SetActive(true);
+            Text1.gameObject.SetActive(true);
+            Background1.gameObject.SetActive(true);
+            xOut.gameObject.SetActive(true);
+            clicked = true;
+        }
+    }
+    void OnButtonClick()
+    {
+        /*if (gameObject.name == "Passenger whatever")"
+        {
+            increment/decrement one of the upgrade variables
+        }*/
+        if (gameObject.name == "Passenger1")
+        {
+            Debug.Log("You just upgraded your passenger 1 power!");
+        }
+    }
+    void OnXClick()
+    {
+        Debug.Log("You clicked the X");
+        Upgrader.gameObject.SetActive(false);
+        Text1.gameObject.SetActive(false);
+        Background1.gameObject.SetActive(false);
+        xOut.gameObject.SetActive(false);
+        clicked = false;
     }
 }
