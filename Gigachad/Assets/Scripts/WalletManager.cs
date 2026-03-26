ff0ec5b311898dd72f28357815aadd1c07c61519
yeehaw-neehaw <neha.sankarkumar@digipen.edu>
2026-03-26 13:04:54 -0700

    wallets + spawnning, collection

---
+++ Gigachad/Assets/Scripts/WalletManager.cs
@@ -0,0 +1,29 @@
+using System.Net.Sockets;
+using UnityEngine;
+
+public class WalletManager : MonoBehaviour
+{
+    public static int WalletPity = 0; // a wallet WILL spawn when this hits 10
+                                      // (10 enemies have not dropped wallets)
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnCollisionEnter2D(Collision2D collision) // when the ticket collides
+    {
+        if (collision.gameObject.CompareTag("Player")) // collides with player
+        {
+            Destroy(gameObject); // ticket disappears
+            PlayerStats.PlayerCash += UnityEngine.Random.Range(1, 6); // cash 1-5
+        }
+    }
+}
