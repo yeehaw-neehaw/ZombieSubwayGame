29bebc6ab6ba9e3fa76ea6d9295a167aff4d9f1b
Bishep Clous <bishep.clous@dit3545us.DIGIPEN.EDU>
2026-04-20 13:26:41 -0700

    soe scene things

--- Gigachad/Assets/Scripts/SceneChanger.cs
+++ Gigachad/Assets/Scripts/SceneChanger.cs
@@ -14,14 +14,17 @@ public class SceneChanger : MonoBehaviour
 {
     public void ChangeScene(string sceneName) // applies to the start button in the intro menu
     {
+        AudioManager.Instance.SFX[13].Play();
         SceneManager.LoadScene(sceneName);
     }
     public void TryAgainChanger(string sceneName)
     {
+        AudioManager.Instance.SFX[13].Play();
         SceneManager.LoadScene(sceneName + PlayerStats.CurrentLevel);
     }
     public void GameExit()
     {
+        AudioManager.Instance.SFX[13].Play();
         Application.Quit();
     }
 }
