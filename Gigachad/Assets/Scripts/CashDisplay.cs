58dd725476498e5959c4180d826f6946bbefcbc0
yeehaw-neehaw <neha.sankarkumar@digipen.edu>
2026-05-04 13:56:28 -0700

    cashdisplay documengaiton

--- Gigachad/Assets/Scripts/CashDisplay.cs
+++ Gigachad/Assets/Scripts/CashDisplay.cs
@@ -1,10 +1,19 @@
+/****************************************************************************
+* File Name: CashDisplay.cs
+* Author: Neha Sankarkumar
+* DigiPen Email: neha.sankarkumar@digipen.edu
+* Course: Game Projects
+*
+* Description: Updates the text elements that display the amount of cash the
+* player currently has.
+****************************************************************************/
 using UnityEngine;
 using UnityEngine.UI;
 using TMPro;
 
 public class CashDisplay : MonoBehaviour
 {
-    public TMP_Text CashUI;
+    public TMP_Text CashUI; // the textbox
 
     // Start is called before the first frame update
     void Start()
