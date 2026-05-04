08042d963f39d24b5c6693ba9016882424bda589
Louiesdad42 <michael.chiodo@digipen.edu>
2026-05-04 12:59:55 -0700

    Finalized Video Thing so it should work in WebGL

--- Gigachad/Assets/Scripts/VideoFinder.cs
+++ Gigachad/Assets/Scripts/VideoFinder.cs
@@ -1,16 +1,27 @@
+using UnityEditor.PackageManager.UI;
 using UnityEngine;
+using UnityEngine.Video;
+using UnityEngine.SceneManagement;
 
 public class VideoFinder : MonoBehaviour
 {
     // Start is called once before the first execution of Update after the MonoBehaviour is created
+    public VideoPlayer videoPlayer;
+    Scene currentScene;
     void Start()
     {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
+        currentScene = SceneManager.GetActiveScene();
+        if (currentScene.name == "Intro Screen")
+        {
+            videoPlayer.url = System.IO.Path.Combine(Application.streamingAssetsPath, "Gigachad_Title_Animation_Final.mp4");
+        }
+        else if (currentScene.name == "Game Over")
+        {
+            videoPlayer.url = System.IO.Path.Combine(Application.streamingAssetsPath, "Death Scene.mp4");
+        }
+        else if (currentScene.name == "On Subway")
+        {
+            videoPlayer.url = System.IO.Path.Combine(Application.streamingAssetsPath, "Gigachad_Subway_Interior_Placeholder_Animated.mp4");
+        }
     }
 }
